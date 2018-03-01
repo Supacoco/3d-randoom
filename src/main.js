db3ec3036802c45ba8a865ba7426c9f363776b85
Nicolas Corral <nico.corral@gmail.com>
2018-03-01 14:39:56 -0500

    feat: 3d visualization ok

--- src/main.js
+++ src/main.js
@@ -4,8 +4,9 @@ import P5 from 'p5'
 const generator = new Xorshift(1337)
 // const generator = new Xorshift(new Date())
 
-const WIDTH = 600
-const ITERATIONS = 1000
+const CANVAS_WIDTH = 800
+const BOX_WIDTH = CANVAS_WIDTH / 2
+const ITERATIONS = 5000
 
 const root = document.createElement('div')
 root.id = 'root'
@@ -20,31 +21,33 @@ const dots = Array(ITERATIONS)
     y: generator.generate(),
     z: generator.generate()
   }))
+
 let currentIndex = 0
+let color
 
 new P5((p5) => {
   p5.setup = () => {
-    p5.createCanvas(500, 500, p5.WEBGL)
-    p5.camera(250, 250, (500 / 2) / p5.tan(p5.PI / 6), 250, 250, 0, 0, 1, 0)
+    p5.createCanvas(CANVAS_WIDTH, CANVAS_WIDTH, p5.WEBGL)
+    p5.camera(BOX_WIDTH / 2, BOX_WIDTH / 2, (BOX_WIDTH / 2) / p5.tan(p5.PI / 6), BOX_WIDTH / 2, BOX_WIDTH / 2, 0, 0, 1, 0)
     p5.colorMode(p5.HSB)
   }
 
   p5.draw = () => {
     currentIndex = 0
     p5.background(0)
-    p5.translate(500 / 2, 500 / 2, -100)
+    p5.translate(BOX_WIDTH / 2, BOX_WIDTH / 2, -BOX_WIDTH / 2)
     p5.rotateY(p5.mouseX / 100)
     p5.stroke(255)
     p5.noFill()
-    p5.box(200)
+    p5.box(BOX_WIDTH)
     p5.noStroke()
     dots.forEach(dot => {
-      const color = p5.map(currentIndex, 0, ITERATIONS, 0, 255)
+      color = p5.map(currentIndex, 0, ITERATIONS, 0, 255)
       p5.fill(color, 255, 255)
       p5.push()
-      p5.translate(-100, 100, 100)
+      p5.translate(-BOX_WIDTH / 2, BOX_WIDTH / 2, BOX_WIDTH / 2)
       p5.sphere(10)
-      p5.translate(dot.x * 200, dot.y * -200, dot.z * -200)
+      p5.translate(dot.x * BOX_WIDTH, dot.y * -BOX_WIDTH, dot.z * -BOX_WIDTH)
       p5.sphere(1)
       p5.pop()
       currentIndex++
