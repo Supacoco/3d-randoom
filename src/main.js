3d4dd7a076a12f6fdae3a1c17605158f855a7c2b
Nicolas Corral <nico.corral@gmail.com>
2018-03-05 16:38:22 -0500

    feat: gaussian rendering

--- src/main.js
+++ src/main.js
@@ -6,7 +6,7 @@ const generator = new Xorshift(1337)
 
 const CANVAS_WIDTH = 800
 const BOX_WIDTH = CANVAS_WIDTH / 2
-const ITERATIONS = 5000
+const ITERATIONS = 1000
 
 const root = document.createElement('div')
 root.id = 'root'
@@ -14,13 +14,49 @@ root.id = 'root'
 document.querySelector('body')
   .appendChild(root)
 
-const dots = Array(ITERATIONS)
-  .fill()
-  .map(() => ({
-    x: generator.generate(),
-    y: generator.generate(),
-    z: generator.generate()
-  }))
+// const dots = Array(ITERATIONS)
+//   .fill()
+//   .map(() => ({
+//     x: generator.generate(),
+//     y: generator.generate(),
+//     z: generator.generate()
+//   }))
+const dots = []
+const test = () => {
+  let x, y, r
+  const numbers = []
+
+  for (let counter = ITERATIONS * 3; counter > 0; counter--) {
+    do {
+      x = 2 * generator.generate() - 1
+      y = 2 * generator.generate() - 1
+      r = x * x + y * y
+    } while (r >= 1 || r === 0)
+
+    numbers.push(
+      x * Math.sqrt(-2 * Math.log(r) / r)
+    )
+  }
+
+  return numbers
+}
+const tmpDots = test()
+while (tmpDots.length) {
+  let meh = tmpDots.splice(0, 3)
+  dots.push({
+    x: meh[0],
+    y: meh[1],
+    // z: 0
+    z: meh[2]
+  })
+  // dots.push({
+  //   x: Math.atan(meh[0]),
+  //   y: Math.atan(meh[1]),
+  //   z: Math.atan(meh[2])
+  // })
+}
+
+console.log(dots)
 
 let currentIndex = 0
 let color
@@ -37,17 +73,23 @@ new P5((p5) => {
     p5.background(0)
     p5.translate(BOX_WIDTH / 2, BOX_WIDTH / 2, -BOX_WIDTH / 2)
     p5.rotateY(p5.mouseX / 100)
-    p5.stroke(255)
-    p5.noFill()
-    p5.box(BOX_WIDTH)
+    p5.rotateX(p5.mouseY / 100)
+    // p5.stroke('grey')
+    // p5.noFill()
+    // p5.sphere(BOX_WIDTH, 8, 8)
+    p5.stroke('white')
+    p5.line(-BOX_WIDTH / 1.25, 0, 0, BOX_WIDTH / 1.25, 0, 0)
+    p5.line(0, -BOX_WIDTH / 1.25, 0, 0, BOX_WIDTH / 1.25, 0)
+    p5.line(0, 0, BOX_WIDTH / 1.25, 0, 0, -BOX_WIDTH / 1.25)
+    p5.fill(128)
     p5.noStroke()
+    // p5.sphere(10)
     dots.forEach(dot => {
       color = p5.map(currentIndex, 0, ITERATIONS, 0, 255)
       p5.fill(color, 255, 255)
       p5.push()
-      p5.translate(-BOX_WIDTH / 2, BOX_WIDTH / 2, BOX_WIDTH / 2)
-      p5.sphere(10)
-      p5.translate(dot.x * BOX_WIDTH, dot.y * -BOX_WIDTH, dot.z * -BOX_WIDTH)
+      // p5.translate(-BOX_WIDTH / 2, BOX_WIDTH / 2, BOX_WIDTH / 2)
+      p5.translate(dot.x * BOX_WIDTH / 4, dot.y * BOX_WIDTH / 4, dot.z * BOX_WIDTH / 4)
       p5.sphere(1)
       p5.pop()
       currentIndex++
